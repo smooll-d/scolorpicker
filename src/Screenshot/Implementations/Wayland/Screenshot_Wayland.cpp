1136d7337ef002e3e90f830c36eff9af85c0e1ca
Jakub Skowron <jakubskowron676@gmail.com>
2025-07-02 18:02:38 +0200

    build: improve cross-platform experience

--- src/Screenshot/Implementations/Wayland/Screenshot_Wayland.cpp
+++ src/Screenshot/Implementations/Wayland/Screenshot_Wayland.cpp
@@ -1,9 +1,15 @@
 #include "Screenshot_Wayland.hpp"
+#include "SDL3/SDL_render.h"
 
 namespace scp
 {
     void Screenshot_Wayland::Take()
     {
         return;
+    }
+
+    SDL_Texture *Screenshot_Wayland::CreateTexture(SDL_Renderer *renderer)
+    {
+        return nullptr;
     }
 }
