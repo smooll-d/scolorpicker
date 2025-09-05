ee36310b08ca50e896d162f57b1f02a9ce8af5e0
Jakub Skowron <jakubskowron676@gmail.com>
2025-09-05 21:22:43 +0200

    build: pull and build dependencies if not installed
    after a couple hours, I've finally figured out how to
    get cmake to recognize whether a dependency is already
    installed or whether it should be pulled and built locally.
    
    i hate cmake so much. next project I'm using meson or
    something else

--- cmake/setup_sdbus.cmake
+++ cmake/setup_sdbus.cmake
@@ -1,12 +1,18 @@
-set(SDBUSCPP_INSTALL OFF)
-set(BUILD_SHARED_LIBS ON)
+find_package(sdbus-c++ CONFIG)
 
-FetchContent_Declare(
-    sdbus-c++
-    GIT_REPOSITORY https://github.com/Kistler-Group/sdbus-cpp.git
-    GIT_TAG v2.1.0
-    GIT_SHALLOW ON
-    GIT_PROGRESS ON
-    OVERRIDE_FIND_PACKAGE
-)
-FetchContent_MakeAvailable(sdbus-c++)
+if (NOT TARGET SDBusCpp::sdbus-c++)
+    set(SCP_SDBUSCPP sdbus-c++ CACHE INTERNAL "SCP_SDBUSCPP")
+    set(SDBUSCPP_INSTALL OFF)
+
+    FetchContent_Declare(
+        sdbus-c++
+        GIT_REPOSITORY https://github.com/Kistler-Group/sdbus-cpp.git
+        GIT_TAG v2.1.0
+        GIT_SHALLOW ON
+        GIT_PROGRESS ON
+        OVERRIDE_FIND_PACKAGE
+    )
+    FetchContent_MakeAvailable(sdbus-c++)
+else()
+    set(SCP_SDBUSCPP SDBusCpp::sdbus-c++ CACHE INTERNAL "SCP_SDBUSCPP")
+endif()
--- cmake/setup_sdl.cmake
+++ cmake/setup_sdl.cmake
@@ -1,51 +1,67 @@
-set(SDL_TESTS OFF)
-set(SDL_EXAMPLES OFF)
-set(SDL_SHARED ON)
-set(SDL_STATIC OFF)
-set(SDL_INSTALL_DOCS OFF)
-set(SDL_INSTALL_TESTS OFF)
-set(SDL_TEST_LIBRARY OFF)
-set(SDL_TESTS OFF)
-set(SDL_DISABLE_INSTALL ON)
+find_package(SDL3 CONFIG)
 
-FetchContent_Declare(
-    SDL3
-    GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-    GIT_TAG        release-3.2.14
-    GIT_SHALLOW    ON
-    GIT_PROGRESS   ON
-)
-FetchContent_MakeAvailable(SDL3)
+if (NOT TARGET SDL3::SDL3)
+    set(SDL_TESTS OFF)
+    set(SDL_EXAMPLES OFF)
+    set(SDL_SHARED ON)
+    set(SDL_STATIC OFF)
+    set(SDL_INSTALL_DOCS OFF)
+    set(SDL_INSTALL_TESTS OFF)
+    set(SDL_TEST_LIBRARY OFF)
+    set(SDL_TESTS OFF)
+    set(SDL_DISABLE_INSTALL ON)
 
-# Disabled for this project
-# FetchContent_Declare(
-#     SDL_mixer
-#     GIT_REPOSITORY https://github.com/libsdl-org/SDL_mixer.git
-#     GIT_TAG        main
-#     GIT_SHALLOW    ON
-#     GIT_PROGRESS   ON
-# )
-# FetchContent_MakeAvailable(SDL_mixer)
+    FetchContent_Declare(
+        SDL3
+        GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
+        GIT_TAG        release-3.2.22
+        GIT_SHALLOW    ON
+        GIT_PROGRESS   ON
+    )
+    FetchContent_MakeAvailable(SDL3)
+endif()
 
-set(SDLIMAGE_SAMPLES OFF)
+find_package(SDL3_image CONFIG)
 
-FetchContent_Declare(
-    SDL_image
-    GIT_REPOSITORY https://github.com/libsdl-org/SDL_image.git
-    GIT_TAG        release-3.2.4
-    GIT_SHALLOW    ON
-    GIT_PROGRESS   ON
-)
-FetchContent_MakeAvailable(SDL_image)
+if (NOT TARGET SDL3_image::SDL3_image)
+    set(SDLIMAGE_SAMPLES OFF)
+
+    FetchContent_Declare(
+        SDL_image
+        GIT_REPOSITORY https://github.com/libsdl-org/SDL_image.git
+        GIT_TAG        release-3.2.4
+        GIT_SHALLOW    ON
+        GIT_PROGRESS   ON
+    )
+    FetchContent_MakeAvailable(SDL_image)
+endif()
 
 # Disabled for this project
-# set(SDLTTF_SAMPLES OFF)
-#
-# FetchContent_Declare(
-#     SDL_ttf
-#     GIT_REPOSITORY https://github.com/libsdl-org/SDL_ttf.git
-#     GIT_TAG        release-3.2.2
-#     GIT_SHALLOW    ON
-#     GIT_PROGRESS   ON
-# )
-# FetchContent_MakeAvailable(SDL_ttf)
+# find_package(SDL3_mixer CONFIG)
+
+# if (NOT TARGET SDL3_mixer::SDL3_mixer)
+    # FetchContent_Declare(
+    #     SDL_mixer
+    #     GIT_REPOSITORY https://github.com/libsdl-org/SDL_mixer.git
+    #     GIT_TAG        main
+    #     GIT_SHALLOW    ON
+    #     GIT_PROGRESS   ON
+    # )
+    # FetchContent_MakeAvailable(SDL_mixer)
+# endif()
+
+# Disabled for this project
+# find_package(SDL3_ttf CONFIG)
+
+# if (NOT TARGET SDL3_ttf::SDL3_ttf)
+    # set(SDLTTF_SAMPLES OFF)
+    #
+    # FetchContent_Declare(
+    #     SDL_ttf
+    #     GIT_REPOSITORY https://github.com/libsdl-org/SDL_ttf.git
+    #     GIT_TAG        release-3.2.2
+    #     GIT_SHALLOW    ON
+    #     GIT_PROGRESS   ON
+    # )
+    # FetchContent_MakeAvailable(SDL_ttf)
+# endif()
