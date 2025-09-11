53f1c681303beb71786ad498398d975e34033ecb
Jakub Skowron <jakubskowron676@gmail.com>
2025-09-11 16:29:22 +0200

    build: add new option and target

---
+++ cmake/localize.cmake
@@ -0,0 +1,21 @@
+foreach(language ${languages})
+    add_custom_command(
+        OUTPUT "${CMAKE_SOURCE_DIR}/docs/man/dist/${language}/scolorpicker.1.gz"
+        DEPENDS "${CMAKE_SOURCE_DIR}/docs/man/${language}/scolorpicker.1"
+        COMMAND ${CMAKE_COMMAND} -E make_directory "${CMAKE_SOURCE_DIR}/docs/man/dist/${language}"
+        COMMAND ${CMAKE_COMMAND} -E copy "${CMAKE_SOURCE_DIR}/docs/man/${language}/scolorpicker.1"
+        "${CMAKE_SOURCE_DIR}/docs/man/dist/${language}/scolorpicker.1"
+        COMMAND gzip "${CMAKE_SOURCE_DIR}/docs/man/dist/${language}/scolorpicker.1"
+    )
+
+    list(APPEND manpages "${CMAKE_SOURCE_DIR}/docs/man/dist/${language}/scolorpicker.1.gz")
+
+    install(FILES "${CMAKE_SOURCE_DIR}/docs/man/dist/${language}/scolorpicker.1.gz"
+        DESTINATION "${CMAKE_INSTALL_MANDIR}/${language}"
+    )
+endforeach()
+
+add_custom_target(dist
+    DEPENDS ${manpages}
+    VERBATIM
+)
