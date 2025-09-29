b4fd0fb3c0987708d5a00cd976df440e3840020d
Jakub Skowron <jakubskowron676@gmail.com>
2025-09-29 11:55:45 +0200

    feat: add window icon
    makes the app a little more complete.
    
    this change hasn't been tested yet, please refrain from making
    issues about it, till tested and fixed.

---
+++ Utils.hpp
@@ -0,0 +1,30 @@
+#ifndef SCP_UTILS
+#define SCP_UTILS
+
+#include <SDL3/SDL.h>
+
+#include <string>
+#include <string_view>
+#include <filesystem>
+
+namespace scp
+{
+    namespace Utils
+    {
+        std::string TRed(std::string_view str, std::string_view ground = "background");
+
+        void TestSDL(SDL_Renderer *renderer);
+
+        unsigned int CountTrailingZeroes(int number);
+
+        int CheckSession();
+
+        std::string Localize(std::string_view messageName);
+
+        std::string ReplacePlaceholder(std::string placeholder, std::string_view actual);
+
+        SDL_Surface *CreateSurfaceFromSTBI(std::filesystem::path filepath);
+    } // namespace Utils
+} // namespace scp
+
+#endif // SCP_UTILS
