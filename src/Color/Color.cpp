e3533253daef8f81ce0bbb068b2c8c9c8c06ad5e
Jakub Skowron <jakubskowron676@gmail.com>
2025-10-13 10:30:24 +0200

    feat: use external programs for clipboard
    SDL3 doesn't work like I expected it to work for copying
    stuff to the clipboard. it didn't work well with hyprland
    and reverting back to the previous method of copying
    works flawlessly, so we're staying with something that
    works better than copying "once in a while".

--- src/Color/Color.cpp
+++ src/Color/Color.cpp
@@ -124,14 +124,72 @@ namespace scp
             return color + '\n';
         else if (appState->cli.GetInfo().output == "clipboard")
         {
-            bool inClipboard = SDL_SetClipboardText(color.c_str());
-            if (!inClipboard)
-            {
-                std::string notInClipboardError = Utils::Localize("Color/not_in_clipboard");
+            int fd[2];
 
-                SDL_Log("%s", Utils::ReplacePlaceholder(notInClipboardError, SDL_GetError()).c_str());
+            if (pipe(fd) == -1)
+            {
+                perror("pipe");
 
                 SDL_Quit();
+            }
+
+            pid_t childPID = fork();
+
+            int status = 0;
+
+            if (childPID == -1)
+            {
+                perror("fork");
+
+                SDL_Quit();
+            }
+            else if (childPID == 0)
+            {
+                if (dup2(fd[0], STDIN_FILENO) == -1)
+                {
+                    perror("dup2");
+
+                    SDL_Quit();
+                }
+
+                close(fd[0]);
+                close(fd[1]);
+
+                const char *argv[3] {};
+
+                if (Utils::CheckSession() == 0)
+                {
+                    argv[0] = "xsel";
+                    argv[1] = "-b";
+                    argv[2] = nullptr;
+                }
+                else if (Utils::CheckSession() == 1)
+                {
+                    argv[0] = "wl-copy";
+                    argv[1] = nullptr;
+                }
+
+                execvp(argv[0], const_cast<char *const *>(argv));
+            }
+            else
+            {
+                close(fd[0]);
+
+                if (write(fd[1], color.c_str(), color.length()) == -1)
+                {
+                    perror("write");
+
+                    SDL_Quit();
+                }
+
+                close(fd[1]);
+
+                if (waitpid(childPID, &status, WNOHANG) == -1)
+                {
+                    perror("waitpid");
+
+                    SDL_Quit();
+                }
             }
         }
 
